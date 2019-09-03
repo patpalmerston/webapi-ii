dabf7058e921f514c8ad995211b608c763df96d8
Pat Palmerston <patpalmerston@gmail.com>
2019-09-03 12:55:43 -0700

    server set up for posts and comments routing

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const server = express();
+
+const postsRouter = require(`./data/routes/postRoutes`)
+const commentsRouter = require(`./data/routes/commentRoutes`)
+
 server.use(express.json());
+server.use('/api/posts', postsRouter)
+server.use('/api/posts', commentsRouter)
 
 // sanity Check
 server.get('/', (req, res) => {
