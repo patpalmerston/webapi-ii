d07d4e2aa00f9b9a7b72a64bf700ed75904c2a67
Pat Palmerston <patpalmerston@gmail.com>
2019-09-03 14:29:34 -0700

    get post comments from comments router

--- data/routes/commentRoutes.js
+++ data/routes/commentRoutes.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require(`../db`);
+
+router.get('/:id/comments', (req, res) => {
+	const id = req.params.id;
+	console.log('comments id', id);
+
+	db.findCommentById(id)
+		.then(comment => {
+			if (comment) {
+				res.status(200).json({ comment });
+			} else {
+				res
+					.status(404)
+					.json({ message: 'The post with the specified ID does not exist.' });
+			}
+		})
+		.catch(err => {
+			res
+				.status(500)
+				.json({ error: 'The comments information could not be retrieved.' });
+		});
+});
 
 module.exports = router;
--- data/routes/postRoutes.js
+++ data/routes/postRoutes.js
@@ -78,18 +78,16 @@ router.delete('/:id', (req, res) => {
 		});
 });
 
-// update post 
+// update post
 
 router.put('/:id', (req, res) => {
 	const { id } = req.params;
 	const { title, contents } = req.body;
 
 	if (!title || !contents) {
-		res
-			.status(400)
-			.json({
-				errorMessage: 'Please provide title and contents for the post.'
-			});
+		res.status(400).json({
+			errorMessage: 'Please provide title and contents for the post.'
+		});
 	}
 
 	db.update(id, { title, contents })
