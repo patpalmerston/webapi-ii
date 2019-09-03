a48a8834b51eac7066e5a53f7cce360cabaf846f
Pat Palmerston <patpalmerston@gmail.com>
2019-09-03 14:42:31 -0700

    get all posts for a comment by id

--- data/routes/commentRoutes.js
+++ data/routes/commentRoutes.js
@@ -3,11 +3,16 @@ const router = express.Router();
 
 const db = require(`../db`);
 
+
+
+
+
+// get all comments by post id
 router.get('/:id/comments', (req, res) => {
 	const id = req.params.id;
 	console.log('comments id', id);
 
-	db.findCommentById(id)
+	db.findPostComments(id)
 		.then(comment => {
 			if (comment) {
 				res.status(200).json({ comment });
@@ -23,5 +28,7 @@ router.get('/:id/comments', (req, res) => {
 				.json({ error: 'The comments information could not be retrieved.' });
 		});
 });
+
+// get comment by id
 
 module.exports = router;
